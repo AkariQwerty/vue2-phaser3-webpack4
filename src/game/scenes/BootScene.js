115d9102aa8cc7ec49bf66a672c87184069d4df1
RubenMR99 <ruben.moreno.romero@gmail.com>
2019-05-09 18:09:16 +0200

    overlay

--- src/game/scenes/BootScene.js
+++ src/game/scenes/BootScene.js
@@ -3,7 +3,14 @@ import sky from '@/game/assets/sky.png';
 import bomb from '@/game/assets/bomb.png';
 import boto from '@/game/assets/botoPlaceholder.png';
 import tile_set from '@/game/assets/tileset.png';
-import tile_fitxes from '@/game/assets/tileset_fichas.png';
+//import tile_fitxes from '@/game/assets/tileset_fichas.png';
+import fitxa_y from '@/game/assets/ficha_1.png';
+import fitxa_b from '@/game/assets/ficha_2.png';
+import fitxa_r from '@/game/assets/ficha_3.png';
+import fitxa_g from '@/game/assets/ficha_4.png';
+import flip from '@/game/assets/flip.png';
+import overlay from '@/game/assets/overlay.png';
+
 
 export default class BootScene extends Scene {
     constructor() {
@@ -15,15 +22,23 @@ export default class BootScene extends Scene {
         this.load.image('bomb', bomb);
 		this.load.image('boto', boto);
 		this.load.image('tile_set', tile_set);
-		this.load.image('tile_fitxes', tile_fitxes);
+		//this.load.image('tile_fitxes', tile_fitxes);
+
+    this.load.image('fitxa_y', fitxa_y);
+    this.load.image('fitxa_b', fitxa_b);
+    this.load.image('fitxa_r', fitxa_r);
+    this.load.image('fitxa_g', fitxa_g);
+
+    this.load.image('flip', flip);
+    this.load.image('overlay', overlay);
         // this.load.audio('thud', ['assets/thud.mp3', 'assets/thud.ogg'])
     }
 
-    create() {		
+    create() {
 		let boto1 = this.add.sprite(400, 300, 'boto').setInteractive();
 		this.add.text(250, 280, 'Play Game', { font: '25px Arial Bold', fill: 'Black' });
 		let th = this
-		
+
 		boto1.on('pointerdown', function (pointer) {
 			this.setTint(0xff0000);
 			th.scene.start('PlayScene');
--- src/game/scenes/PlayScene.js
+++ src/game/scenes/PlayScene.js
@@ -1,5 +1,5 @@
 import { Scene } from 'phaser';
-
+let th;
 let nJugadors = 4
 let fitxesSprite = []
 let fitxes = [];
@@ -11,13 +11,14 @@ let tiles;
 let layer;
 let map;
 
+let casellaPosible = [];
+let cruzeta = [];
+
 let fitxesTiles;
 let fitxesMap;
 let dibuixa = 3
 let midaTile = 64
 let jugadorAct = 0
-
-let posicioJ1 = []
 
 function coordenades(x, y) {
     this.x = x;
@@ -33,18 +34,30 @@ export default class PlayScene extends Scene {
     console.log("Starting PlayScene ...");
     //let i = this.add.image(400, 300, 'tile_set');
 	 // Load a map from a 2D array of tile indices
-
+   th = this;
 	 let i;
+   //Crear Tauler
 	 for ( i = 0; i < taulerAlcada; i ++){
 		 let j;
-		 tauler[i] = []
-		 for ( j = 0; j < taulerAmplada; j ++){
+		 tauler[i] = [];
+		 for ( j = 0; j <  taulerAmplada; j ++){
 			tauler[i].push(Phaser.Math.Between(0, 3));
 			if ( i == 0 || i == taulerAlcada - 1){
 				tauler[i][j] = 5;
 			}
 		 }
 	 }
+   //Caselles Posibles
+   for ( i = 0; i <= taulerAmplada; i++) {
+     casellaPosible[i] = [];
+     let j;
+     for ( j = 0; j < taulerAmplada; j ++){
+       casellaPosible[i].push(true);
+       if ( j == 1 || j == taulerAlcada){
+         casellaPosible[i][j] = false;
+ 			}
+     }
+   }
 	 tauler[0][0] = 4;
 	 tauler[0][taulerAmplada - 1] = 4;
 	 tauler[taulerAlcada - 1][0] = 4;
@@ -61,29 +74,35 @@ export default class PlayScene extends Scene {
 	tiles = map.addTilesetImage('tile_set');
 	layer = map.createStaticLayer(0, tiles);
 
+  //Crea els sprites de jugadors
   for (i = 0; i < nJugadors; i++) {
-    fitxesSprite.push(this.add.image(midaTile, midaTile, 'tile_fitxes'));
+    var nouSprite;
     switch (i) {
       case 0:
-        console.log(i);
-        fitxesSprite[i].frame.cutHeight = midaTile; fitxesSprite[i].frame.cutWidth = midaTile;
-        fitxesSprite[i].x = midaTile; fitxesSprite[i].y = midaTile;
+        nouSprite = this.add.image(midaTile, midaTile, 'fitxa_y');
+        nouSprite.setScale(.25);
+        nouSprite.setOrigin(1);
+        fitxesSprite.push(nouSprite);
         break;
       case 1:
-        console.log(i);
-        fitxesSprite[i].frame.cutX = midaTile; fitxesSprite[i].frame.cutY = midaTile;
-        fitxesSprite[i].x = taulerAmplada * midaTile; fitxesSprite[i].y = midaTile;
+        nouSprite = this.add.image(taulerAmplada * midaTile, midaTile, 'fitxa_b');
+        nouSprite.setScale(.25);
+        nouSprite.setOrigin(1);
+        fitxesSprite.push(nouSprite);
         break;
       case 2:
-        fitxesSprite[i].frame.cutHeight = midaTile; fitxesSprite[i].frame.cutWidth = midaTile;
-        fitxesSprite[i].x = midaTile; fitxesSprite[i].y = taulerAlcada * midaTile;
+        nouSprite = this.add.image(midaTile, taulerAlcada * midaTile, 'fitxa_r');
+        nouSprite.setScale(.25);
+        nouSprite.setOrigin(1);
+        fitxesSprite.push(nouSprite);
         break;
       case 3:
-        fitxesSprite[i].frame.cutHeight = midaTile; fitxesSprite[i].frame.cutWidth = midaTile;
-        fitxesSprite[i].x = taulerAmplada * midaTile; fitxesSprite[i].y = taulerAlcada * midaTile;
+        nouSprite = this.add.image(taulerAmplada * midaTile, taulerAlcada * midaTile, 'fitxa_g');
+        nouSprite.setScale(.25);
+        nouSprite.setOrigin(1);
+        fitxesSprite.push(nouSprite);
         break;
     }
-    fitxesSprite[i].frame.updateUVs();
   }
 
   this.input.on('pointerdown', function (pointer) {
@@ -94,21 +113,72 @@ export default class PlayScene extends Scene {
   }
 
   update () {
-	/*
-	map = this.make.tilemap({ data: tauler, tileWidth: midaTile, tileHeight: midaTile });
-	tiles = map.addTilesetImage('tile_set');
-	layer = map.createStaticLayer(0, tiles);*/
+
   }
 
 }
 
   function mou (jugador, posicio) {
-    var spriteMoute = fitxesSprite[jugador]
-    spriteMoute.x = (Math.floor(posicio.x / midaTile) + 1) * midaTile;
-    spriteMoute.y = (Math.floor(posicio.y / midaTile) + 1) * midaTile;
-    jugadorAct += 1;
-    if (jugadorAct == nJugadors) {
-      jugadorAct = 0;
+    var spriteMoure = fitxesSprite[jugador]
+    var nouPosX = (Math.floor(posicio.x / midaTile) + 1);
+    var nouPosY = (Math.floor(posicio.y / midaTile) + 1);
+    /*console.log(nouPosX);
+    console.log(nouPosY);
+    console.log(casellaPosible[nouPosX][nouPosY]);
+    console.log(" ");*/
+    if ( casellaPosible[nouPosX][nouPosY] ) {
+      casellaPosible[nouPosX][nouPosY] = false;
+      flip_casella(nouPosX * midaTile, nouPosY * midaTile);
+      spriteMoure.x = nouPosX * midaTile;
+      spriteMoure.y = nouPosY * midaTile;
+      spriteMoure.setDepth(1);
+      jugadorAct += 1;
+      if (jugadorAct == nJugadors) {
+        jugadorAct = 0;
+      }
+      marca_posible(fitxesSprite[jugadorAct].x, fitxesSprite[jugadorAct].y, Phaser.Math.Between(1, 6));
     }
-    //fitxesSprite[0].frame.updateUVs();
+  }
+
+  function flip_casella (casellaX, casellaY) {
+    th.add.image(casellaX - midaTile / 2, casellaY - midaTile / 2 , 'flip').setDepth(0);
+  }
+
+  function marca_posible(casellaX, casellaY, numDau = 5) {
+    var posXtauler = (Math.floor(casellaX / midaTile) + 1);
+    var posYtauler = (Math.floor(casellaY / midaTile) + 1);
+    console.log(posXtauler);
+    console.log(posYtauler);
+    console.log(" ");
+    for (var i = 0; i < cruzeta.length; i++) {
+      cruzeta[i].destroy();
+    }
+    if (posYtauler - numDau > 2 && casellaPosible[posXtauler][posYtauler - numDau + 1]){
+      var verticalTop = th.add.image(casellaX, casellaY - midaTile, 'overlay');
+      verticalTop.setScale(1, numDau);
+      verticalTop.setOrigin(1);
+      cruzeta.push(verticalTop);
+    }
+
+    if (posYtauler + numDau < taulerAlcada && casellaPosible[posXtauler][posYtauler + numDau]){
+      var verticalBot = th.add.image(casellaX, casellaY + (numDau * midaTile), 'overlay');
+      verticalBot.setScale(1, numDau + 1);
+      verticalBot.setOrigin(1);
+      cruzeta.push(verticalBot);
+    }
+
+    if (posXtauler - numDau < taulerAmplada - 1 && casellaPosible[posXtauler + numDau][posYtauler - numDau]){
+      var horizontalR = th.add.image(casellaX - midaTile, casellaY, 'overlay');
+      horizontalR.setScale(numDau, 1);
+      horizontalR.setOrigin(1);
+      cruzeta.push(horizontalR);
+    }
+
+
+    if (posXtauler - numDau > 0 && casellaPosible[posXtauler - numDau][posYtauler]){ //Works
+      var horizontalL = th.add.image(casellaX + (midaTile * numDau), casellaY, 'overlay');
+      horizontalL.setScale(numDau, 1);
+      horizontalL.setOrigin(1);
+      cruzeta.push(horizontalL);
+    }
 }
